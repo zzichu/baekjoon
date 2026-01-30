255d3b26126a043d8f8768d7f54a97fd8d4ded98
Lee Jisu <114422496+zzichu@users.noreply.github.com>
2026-01-31 00:03:02 +0900

    [Programmers] 완주하지못한선수

---
+++ src/Programmers/완주하지못한선수.java
@@ -0,0 +1,28 @@
+package src.Programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+
+        for (String s : map.keySet()) {
+            if (map.get(s) > 0) {
+                answer = s;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
