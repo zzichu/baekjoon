3fb405e0fa8773566c950b71a22396d31d262b6e
Lee Jisu <114422496+zzichu@users.noreply.github.com>
2026-03-04 09:56:29 +0900

    [Bronze] BOJ1076 저항

---
+++ src/Bronze/BOJ1076.java
@@ -0,0 +1,28 @@
+package src.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1076 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String,String> map1 = new HashMap<>(){{
+            put("black","0");
+            put("brown", "1");
+            put("red", "2");
+            put("orange", "3");
+            put("yellow", "4");
+            put("green", "5");
+            put("blue", "6");
+            put("violet", "7");
+            put("grey", "8");
+            put("white", "9");
+        }};
+        String color1 = br.readLine();
+        String color2 = br.readLine();
+        String color3 = br.readLine();
+        long ans = Long.parseLong(map1.get(color1) + map1.get(color2));
+        ans *= Math.pow(10.0, (double) Integer.parseInt(map1.get(color3)));
+        System.out.print(ans);
+    }
+}
