a9d07dda99b4f561d4cad368887239038548cb55
Lee Jisu <114422496+zzichu@users.noreply.github.com>
2026-03-11 21:54:58 +0900

    [Bronze] BOJ1085 직사각형에서 탈출

---
+++ src/Bronze/BOJ1085.java
@@ -0,0 +1,13 @@
+package src.Bronze;
+
+import java.util.Scanner;
+public class BOJ1085 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		System.out.println(Math.min(Math.min(Math.min(x, y), w-x), h-y));
+	}
+}
