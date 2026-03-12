aafda2116985e1b36ef6c760318aecee1a4cf66d
Lee Jisu <114422496+zzichu@users.noreply.github.com>
2026-03-12 22:37:41 +0900

    [Silver] BOJ1094 막대기

---
+++ src/Silver/BOJ1094.java
@@ -0,0 +1,32 @@
+package src.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1094 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int count = 0;
+		int[] arr = {64, 32, 16, 8, 4, 2, 1};
+		int sum = 0;
+		
+		for(int i = 0; i < 7; i++) {
+			if(arr[i] <= X) {
+				sum += arr[i];
+				count++;
+			}
+
+			if(sum > X) {
+				sum -= arr[i];
+				count--;
+			}
+
+			if(sum == X) break;
+		}
+		System.out.println(count);
+	}
+
+}
