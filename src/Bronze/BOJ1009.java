fe1e94320ee0421a4255ac04125371c7cf3aa010
Lee Jisu <114422496+zzichu@users.noreply.github.com>
2026-03-26 11:07:54 +0900

    [Bronze] BOJ1009 분산처리

---
+++ src/Bronze/BOJ1009.java
@@ -0,0 +1,22 @@
+package src.Bronze;
+
+import java.util.*;
+
+public class BOJ1009{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			
+			int result=1;
+			for(int j=0;j<b;j++) {
+				result=(result*a)%10;
+			}
+			if(result==0) System.out.println("10");
+			else System.out.println(result);
+		}
+	}
+}
